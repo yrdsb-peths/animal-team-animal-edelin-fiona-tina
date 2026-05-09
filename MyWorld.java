6f786634da819cb6b207782eb3b50350c718ca21
edelinliu <440009774@gapps.yrdsb.ca>
2026-05-09 14:49:14 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -7,6 +7,8 @@ import greenfoot.*;
  */
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
         super(600, 400, 1);
         
@@ -15,11 +17,21 @@ public class MyWorld extends World {
         addObject(owl, 300, 200);
         
         //Create a label
-        Label scoreLabel = new Label(0, 80);
+        scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
         
         createMath();
     }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
     /**
      * create new math for our owl to get smarter YAY at the top
      */
