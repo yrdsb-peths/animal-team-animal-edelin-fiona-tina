45a18a90a4c112023683cfefc6f303880a294c86
Fiona-091305 <349460105@gapps.yrdsb.ca>
2026-05-11 11:53:22 -0400

    up to 2.8

--- Owl.java
+++ Owl.java
@@ -9,30 +9,79 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Owl extends Actor
 {
     GreenfootSound owlSound = new GreenfootSound("owlhooting.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[7];
+    GreenfootImage[] idleLeft = new GreenfootImage[7];
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    /**
+     * constructor the code that gets run the first time when object created
+     */
+    public Owl()
+    {
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/owlidle/idle" + i + ".png");
+            idleRight[i].scale(100,100);
+        }
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/owlidle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100,100);
+        }
+        
+        animationTimer.mark();
+        //intially facing right
+        setImage(idleRight[0]);
+    }
+    
+    /**
+     * animate the owl
+     */
+    int imageIndex = 0;
+    public void animateOwl()
+    {
+        if(animationTimer.millisElapsed() < 200)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length; 
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+    
     /**
      * Act - do whatever the Owl wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Owl()
-    {
-        GreenfootImage image = getImage();
-        image.scale(100, 100);
-        setImage(image);
-    }
+    
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-1);
+            move(-2);
+            facing = "right";
         }
         else if(Greenfoot.isKeyDown("Right"))
         {
-            move(1);
+            move(2);
+            facing ="left";
         }
         
         //check if its touchign any math books
         eat();
+        // animate owl
+        animateOwl();        
     }
     
     /**
